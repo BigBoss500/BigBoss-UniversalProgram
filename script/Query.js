a7b3ba083d52e1970d01b4fcdcb6764feb31739e
bigboss <42679931+Dmitry8574@users.noreply.github.com>
2019-03-26 11:08:46 +0500

    Update Query.js

--- script/Query.js
+++ script/Query.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function () {
     jQuery('#run').click(function () {
-        require('child_process').exec('start ./VK_UserInfo.exe');
+        require('child_process').exec('start VK_UserInfo.exe');
     });
 });

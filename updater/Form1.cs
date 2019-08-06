6e46565e3a8397bd28f0887bbf73091c5cd49a06
BigBoss500 <44552715+BigBoss500@users.noreply.github.com>
2019-08-06 15:53:05 +0500

    Права администратора для updater.exe

--- updater/Form1.cs
+++ updater/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
+using System.Security.Permissions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
